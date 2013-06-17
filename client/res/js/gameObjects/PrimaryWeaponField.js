45150ddd7c32a9b5757952bae9f741121a6c8ade
SquareFeet <luke.t.moody@googlemail.com>
2013-06-17 19:51:13 +0100

    Updated PrimaryWeaponField.js

--- client/res/js/gameObjects/PrimaryWeaponField.js
+++ client/res/js/gameObjects/PrimaryWeaponField.js
@@ -9,6 +9,7 @@
 	    initialize: function( options ) {
 	    	this.hasFired = [];
 	    	this.origins = [];
+	    	this.pool = [];
 
 	        this.geometry = new THREE.Geometry();
 	        this.material = new THREE.ParticleBasicMaterial({
@@ -16,7 +17,15 @@
 		        size: 10
 		    });
 
-	        this.makeVertices();
+	        this.geometry.vertices.push(
+	        	new THREE.Vector3(
+	        		Number.POSITIVE_INFINITY,
+	        		Number.POSITIVE_INFINITY,
+	        		Number.POSITIVE_INFINITY
+	        	)
+	        );
+
+	        this.createVertices();
 
 	        this.particleSystem = new THREE.ParticleSystem(this.geometry, this.material);
 		    this.particleSystem.dynamic = true;
@@ -25,9 +34,16 @@
 		    this.renderables.push( this.particleSystem );
 	    },
 
-	    makeVertices: function() {
+	    getFromPool: function() {
+	    	return this.pool.pop();
+	    },
+	    returnToPool: function( i ) {
+	    	this.pool.push( i );
+	    },
+
+	    createVertices: function() {
 	    	for(var i = 0; i < 1000; ++i) {
-	    		this.geometry.vertices.push(
+				this.geometry.vertices.push(
 		        	new THREE.Vector3(
 		        		Number.POSITIVE_INFINITY,
 		        		Number.POSITIVE_INFINITY,
@@ -35,40 +51,59 @@
 		        	)
 		        );
 		        this.origins.push( this.makeOrigin() );
+		        this.pool.push( i );
 	    	}
 	    },
 
 	    makeOrigin: function() {
+	    	var obj3d = new THREE.Object3D();
+	    	obj3d.useQuaternion = true;
+
 	    	return {
-	    		quaternion: new THREE.Quaternion(),
-	    		position: new THREE.Vector3()
+	    		object3d: obj3d,
+	    		player: null,
+	    		zPos: 1
 	    	};
 	    },
 
-	    setQuaternion: function( origin, quaternion ) {
-	    	origin.quaternion = quaternion;
-	    },
-
-
 	    fire: function() {
-	    	console.log('yay')
-	    	this.hasFired.push(0);
-	    	// this.geometry.vertices[0] = new THREE.Vector3();
-	    	this.geometry.vertices[0] = sceneManager.middleground.camera.position.clone();
-	    	this.geometry.vertices[0].setEulerFromQuaternion( sceneManager.middleground.camera.quaternion );
+	    	var index = this.getFromPool(),
+	    		origin = this.origins[ index ],
+	    		obj3d = origin.object3d;
+
+	    	obj3d.position = sceneManager.middleground.camera.position.clone();
+	    	obj3d.quaternion = sceneManager.middleground.camera.quaternion.clone();
+	    	obj3d.translateZ( -50 );
+	    	origin.zPos = -50;
+	    	origin.player = players[userName];
+	    	this.geometry.vertices[index] = origin.position;
+
+	    	this.hasFired.push(index);
 	    },
 
 	    tick: function() {
 	    	this.geometry.verticesNeedUpdate = true;
 
 	    	if(this.hasFired.length) {
-	    		this.geometry.vertices[0].z -= 100;
+	    		var i = this.hasFired.length;
+
+	    		while(--i >= 0) {
+	    			var index = this.hasFired[i],
+	    				origin = this.origins[ index ];
+
+	    			if( origin.zPos < -500 ) {
+	    				origin.object3d.position.x = -Number.POSITIVE_INFINITY;
+	    				origin.object3d.position.y = -Number.POSITIVE_INFINITY;
+	    				origin.object3d.position.z = -Number.POSITIVE_INFINITY;
+	    				this.returnToPool( index );
+	    				this.hasFired.splice(i, 1);
+	    			}
+	    			else {
+	    				origin.zPos -= 5;
+	    				origin.object3d.translateZ( origin.zPos );
+	    			}
+	    		}
 	    	}
-
-	    	// this.particleSystem.quaternion = camera.quaternion.clone();
-	    },
-
-	    getNewParticlePosition: function() {
 
 	    },
 
